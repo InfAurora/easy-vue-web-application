ad4d3a57a2d1d7e906faa719623b70f07ad524af
InfAurora <albajo0123@gmail.com>
2021-06-03 04:52:43 -0500

    Restarted project to follow a non outdated 2021 tutorial

--- vue-web-application/src/App.vue
+++ vue-web-application/src/App.vue
@@ -1,40 +1,60 @@
 <template>
-  <div>
-    <Todos v-bind:todos="todos" />
+  <div class="container">
+    <Header />
+    
   </div>
 </template>
 
 <script>
-import Todos from './components/Todos';
+import Header from "./components/Header";
 export default {
   name: "App",
   components: {
-    Todos
+    Header
   },
-  data() {
-    return {
-      todos: [
-        {
-          id: 1,
-          title: "Todo One",
-          completed: false
-        },
-        {
-          id: 2,
-          title: "Todo Two",
-          completed: true
-        },
-        {
-          id: 3,
-          title: "Todo Three",
-          completed: false
-        }
-      ]
-    }
-  }
 };
 </script>
 
 <style lang="sass">
+@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@300;400&display=swap')
+*
+  box-sizing: border-box
+  margin: 0
+  padding: 0
+
+body
+  font-family: 'Poppins', sans-serif
+
+.container
+  max-width: 500px
+  margin: 30px auto
+  overflow: auto
+  min-height: 300px
+  border: 1px solid steelblue
+  padding: 30px
+  border-radius: 5px
+
+.btn
+  display: inline-block
+  background: #000
+  color: #fff
+  border: none
+  padding: 10px 20px
+  margin: 5px
+  border-radius: 5px
+  cursor: pointer
+  text-decoration: none
+  font-size: 15px
+  font-family: inherit
+
+.btn:focus
+  outline: none
+
+.btn:active
+  transform: scale(0.98)
+
+.btn-block
+  display: block
+  width: 100%
 
 </style>
