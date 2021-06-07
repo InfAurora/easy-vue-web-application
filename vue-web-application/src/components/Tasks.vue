41faf4a51f52a8b39f20c69350df2bb215a8a695
InfAurora <albajo0123@gmail.com>
2021-06-06 23:59:29 -0800

    On clicking of x icon in task.vue, it will emit delete-task with task id; once it hits app layer it will call method

--- vue-web-application/src/components/Tasks.vue
+++ vue-web-application/src/components/Tasks.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <div :key="task.id" v-for="task in tasks">
-      <Task :task="task"/>
+      <Task @delete-task='$emit("deleteTask", task.id)' :task="task"/>
     </div>
   </div>
 </template>
